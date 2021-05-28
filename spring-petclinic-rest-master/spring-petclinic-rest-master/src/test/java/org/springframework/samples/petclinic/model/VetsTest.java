7a32711df646a460091be1cde9455b4062dadb18
RenuGhumare <70112364+RenuGHumare@users.noreply.github.com>
2021-05-28 08:38:48 +0200

    Update VetsTest.java

--- spring-petclinic-rest-master/spring-petclinic-rest-master/src/test/java/org/springframework/samples/petclinic/model/VetsTest.java
+++ spring-petclinic-rest-master/spring-petclinic-rest-master/src/test/java/org/springframework/samples/petclinic/model/VetsTest.java
@@ -1,6 +1,7 @@
 package org.springframework.samples.petclinic.model;
 import static org.junit.jupiter.api.Assertions.*;
-
+import java.util.ArrayList;
+import java.util.List;
 
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
@@ -12,37 +13,31 @@ public class VetsTest {
     Vet vet1,vet2,vet3;
 
     @BeforeEach
-    void initMethod() { testVets = new Vets(); }
+    void initVets() { testVets = new Vets(); }
 
     @Test
     @DisplayName("Test to check If List is Empty")
     void emptyVetsList() {
         assertNotNull(testVets.getVetList());
         assertEquals(0,testVets.getVetList().size());
+        assertTrue(testVets.getVetList().isEmpty());
     }
 
     @Test
-    @DisplayName("Test to Add New Vets to the List")
-    void addVetsList() {
+    @DisplayName("Test to get Vet List")
+    void getVetList() {
+            vet1 = new Vet();
+            vet2 = new Vet();
+            vet3 = new Vet();
+            testVets.getVetList().add(vet1);
+            testVets.getVetList().add(vet2);
+            testVets.getVetList().add(vet3);
 
-        vet1 = new Vet(); vet2 = new Vet(); vet3 = new Vet();
-        testVets.getVetList().add(vet1);
-        testVets.getVetList().add(vet2);
-        assertEquals(2,testVets.getVetList().size());
-        assertNotEquals(5,testVets.getVetList().size());
-        assertEquals(vet1,testVets.getVetList().get(0));
-        assertEquals(vet2,testVets.getVetList().get(1));
-
-        testVets.getVetList().add(vet3);
         assertEquals(3,testVets.getVetList().size());
-        assertEquals(vet3,testVets.getVetList().get(2));
-        
+        assertFalse(testVets.getVetList().isEmpty());
     }
 
     @AfterEach
     void teardown() { testVets  = null; vet1 = null; vet2 = null; vet3 = null; }
-
-
-
 
 }
